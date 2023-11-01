31789e8fffc44b2d3b8add6ff3ae6dbff29f7dd3
akira2410 <akira2410.luminous@gmail.com>
2023-11-01 16:21:56 +0900

    docs:説明コメント

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
  
+// 親コンポーネントのテンプレートリテラルによるスタイル記述(tailwind)に子コンポーネントのスタイルをマージする
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
